f46216102480e40d1088db5fcfaadae9f368eaaf
Involvex <lukaswill97@gmail.com>
2025-09-19 22:12:09 +0200

    fix: Manually correct vitest.config.js newlines

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,17 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({\n  test: {\n    coverage: {\n      provider: \'v8\',\n      reporter: [\'text\', \'json\', \'html\'],\n    },\n    environment: \'node\',\n    transformMode: {\n      web: [/\.js$/],\n    },\n  },\n  resolve: {\n    extensions: [\'.js\', \'.ts\', \'.jsx\', \'.tsx\', \'.json\'],\n  },\n});
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+    environment: 'node',
+    transformMode: {
+      web: [/\.js$/],
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+});
